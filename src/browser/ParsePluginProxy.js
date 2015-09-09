1433707a9a03e5b57d87f1c97b7dbc07f3d3c2e5
Denis Babineau <denis.babineau@gmail.com>
2015-09-09 18:32:45 -0300

    browser: fix race condition between readConfig/initialize

--- src/browser/ParsePluginProxy.js
+++ src/browser/ParsePluginProxy.js
@@ -2,27 +2,52 @@ var configHelper = cordova.require('cordova/confighelper');
 
 var appId = undefined;
 var jsKey = undefined;
+var initialized = false;
+var initialize_callback = undefined;
 
 /**
  * Read keys from config.xml
  */
 (function init() {
+    function ready() {
+        initialized = true;
+        if (initialize_callback) {
+            initialize_callback();
+            initialize_callback = undefined;
+        }
+    }
+
     configHelper.readConfig(function(config) {
-        appId = config.getPreferenceValue("APP_ID");
-        jsKey = config.getPreferenceValue("JS_KEY");
+        // Gets called twice under some circumstances ?
+        if (!initialized) {
+            appId = config.getPreferenceValue("APP_ID");
+            jsKey = config.getPreferenceValue("JS_KEY");
+            ready();
+        }
     }, function(err) {
-        console.error(err);
+        if (!initialized) {
+            console.error(err);
+            ready();
+        }
     });
 })();
 
 module.exports = {
     initialize: function (success, error) {
-        if (appId && jsKey) {
-            if (success) {
-                success({ parse_app_id: appId, parse_js_key: jsKey });
+        var cb = function(success, error) {
+            if (appId && jsKey) {
+                if (success) {
+                    success({ parse_app_id: appId, parse_js_key: jsKey });
+                }
+            } else if (error) {
+                error("APP_ID and/or JS_KEY is not defined in browser preferences");
             }
-        } else if (error) {
-            error("APP_ID and/or JS_KEY is not defined in browser preferences");
+        };
+
+        if (!initialized) {
+            initialize_callback = cb.bind(this, success, error);
+        } else {
+            cb(success, error);
         }
     }
 };
